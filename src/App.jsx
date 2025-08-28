f5681a995ab069433581180e1017700ff258afaa
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-08-28 17:21:04 -0500

    ✨feat: envolvemos nuesta app con el contexto

--- src/App.jsx
+++ src/App.jsx
@@ -293,90 +293,96 @@ function App() {
   const searchRef = useRef(null);
 
   return (
-    <div className="flex h-screen">
-      {isBlocked && (
-        <AuthModal/>
-        )}
-      {/* ESCRITORIO */}
-      <div className='hidden md:block'>
-        {isCompact ? (
-          <SideBarCompact 
-            onExpand = {() => setIsCompact(false)}
-            onNewChat={handleNewChat}
-            onMenuSearchOpen={() => setIsMenuSearchOpen(true)}
-            onMenuSearchClose={() => setIsMenuSearchOpen(false)}
-            isMenuSearchOpen={isMenuSearchOpen}
-            setIsMenuSearchOpen={setIsMenuSearchOpen}
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-            onChatSelect={handleChatSelect}
-            onDeleteChat={handleDeleteChat}
-            chats={chats}
-            searchRef={searchRef}
-            />
-        ) : (
-          <SideBar 
-            onCollapse = {() => setIsCompact(true)}
-            chats={chats}
-            currentChatID={currentChatID}
-            onNewChat={handleNewChat}
-            onChatSelect={handleChatSelect}
-            onDeleteChat={handleDeleteChat}
-            onMenuSearchOpen={() => setIsMenuSearchOpen(true)}
-            onMenuSearchClose={() => setIsMenuSearchOpen(false)}
-            isMenuSearchOpen={isMenuSearchOpen}
-            setIsMenuSearchOpen={setIsMenuSearchOpen}
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-            searchRef={searchRef}
-            setIsMobileOpen={setIsMobileOpen}
-            className="hidden md:flex"
-            />
-        )}
+    <>
+    <ToastContainer/>
+    <AuthProvider>
+      <RoutesIndex/>
+      <div className="flex h-screen">
+        {isBlocked && (
+          <AuthModal/>
+          )}
+        {/* ESCRITORIO */}
+        <div className='hidden md:block'>
+          {isCompact ? (
+            <SideBarCompact 
+              onExpand = {() => setIsCompact(false)}
+              onNewChat={handleNewChat}
+              onMenuSearchOpen={() => setIsMenuSearchOpen(true)}
+              onMenuSearchClose={() => setIsMenuSearchOpen(false)}
+              isMenuSearchOpen={isMenuSearchOpen}
+              setIsMenuSearchOpen={setIsMenuSearchOpen}
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+              onChatSelect={handleChatSelect}
+              onDeleteChat={handleDeleteChat}
+              chats={chats}
+              searchRef={searchRef}
+              />
+          ) : (
+            <SideBar 
+              onCollapse = {() => setIsCompact(true)}
+              chats={chats}
+              currentChatID={currentChatID}
+              onNewChat={handleNewChat}
+              onChatSelect={handleChatSelect}
+              onDeleteChat={handleDeleteChat}
+              onMenuSearchOpen={() => setIsMenuSearchOpen(true)}
+              onMenuSearchClose={() => setIsMenuSearchOpen(false)}
+              isMenuSearchOpen={isMenuSearchOpen}
+              setIsMenuSearchOpen={setIsMenuSearchOpen}
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+              searchRef={searchRef}
+              setIsMobileOpen={setIsMobileOpen}
+              className="hidden md:flex"
+              />
+          )}
 
+        </div>
+
+        {/* MÓVIL */}
+        <div className="block md:hidden">
+          {isMobileOpen && (
+            <div className='fixed inset-0 z-20 flex'>
+              <div
+                className="fixed inset-0 bg-black bg-opacity-50"
+                onClick={() => setIsMobileOpen(false)}>
+              </div>
+              <div className='relative z-30 w-64 h-full bg-gray-800'>
+                <SideBar 
+                  onClose={() => setIsMobileOpen(false)}
+                  setIsMobileOpen={setIsMobileOpen}
+                  chats={chats}
+                  currentChatID={currentChatID}
+                  onNewChat={handleNewChat}
+                  onChatSelect={handleChatSelect}
+                  onDeleteChat={handleDeleteChat}
+                  onMenuSearchOpen={() => setIsMenuSearchOpen(true)}
+                  onMenuSearchClose={() => setIsMenuSearchOpen(false)}
+                  isMenuSearchOpen={isMenuSearchOpen}
+                  setIsMenuSearchOpen={setIsMenuSearchOpen}
+                  searchTerm={searchTerm}
+                  setSearchTerm={setSearchTerm}
+                  searchRef={searchRef}
+                  />
+              </div>
+            </div>
+          )}
+        </div>        
+          <Chats 
+            messages={messages}
+            setMessages={setMessages}
+            input={input}
+            setInput={setInput}
+            isLoading={isLoading}
+            setIsLoading={setIsLoading}
+            handleSendMessage={handleSendMessage}
+            onOpenMenu={() => setIsMobileOpen(true)}
+            isBlocked={isBlocked}
+          /> 
       </div>
-
-      {/* MÓVIL */}
-      <div className="block md:hidden">
-        {isMobileOpen && (
-          <div className='fixed inset-0 z-20 flex'>
-            <div
-              className="fixed inset-0 bg-black bg-opacity-50"
-              onClick={() => setIsMobileOpen(false)}>
-            </div>
-            <div className='relative z-30 w-64 h-full bg-gray-800'>
-              <SideBar 
-                onClose={() => setIsMobileOpen(false)}
-                setIsMobileOpen={setIsMobileOpen}
-                chats={chats}
-                currentChatID={currentChatID}
-                onNewChat={handleNewChat}
-                onChatSelect={handleChatSelect}
-                onDeleteChat={handleDeleteChat}
-                onMenuSearchOpen={() => setIsMenuSearchOpen(true)}
-                onMenuSearchClose={() => setIsMenuSearchOpen(false)}
-                isMenuSearchOpen={isMenuSearchOpen}
-                setIsMenuSearchOpen={setIsMenuSearchOpen}
-                searchTerm={searchTerm}
-                setSearchTerm={setSearchTerm}
-                searchRef={searchRef}
-                />
-            </div>
-          </div>
-        )}
-      </div>        
-        <Chats 
-          messages={messages}
-          setMessages={setMessages}
-          input={input}
-          setInput={setInput}
-          isLoading={isLoading}
-          setIsLoading={setIsLoading}
-          handleSendMessage={handleSendMessage}
-          onOpenMenu={() => setIsMobileOpen(true)}
-          isBlocked={isBlocked}
-        /> 
-    </div>
+    </AuthProvider>
+    </>
   );
 }
 
