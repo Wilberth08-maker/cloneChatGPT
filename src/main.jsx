f28260c555e418ad73b4ad2d472f5d22b94f3629
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-08-28 17:27:39 -0500

    ✨ feat: envolvemos la aplicación con el router

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App';
 import { DarkModeProvider } from './context/DarkModeContext';
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
   <StrictMode>
     <DarkModeProvider>
       <App />
     </DarkModeProvider>
   </StrictMode>
+  </BrowserRouter>
 );
