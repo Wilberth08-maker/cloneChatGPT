90d4b90053165153505c3efa2adb0c6d88556030
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-08-28 19:27:17 -0500

    ✨fix: correción de envolver mi app en authProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App';
 import { DarkModeProvider } from './context/DarkModeContext';
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
     <DarkModeProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </DarkModeProvider>
   </StrictMode>
   </BrowserRouter>
