f64052864f8ed8419977ca013c445c3d5af31ec3
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-09-01 18:03:42 -0500

    ✨refactor: refactorización de home para el uso de chatContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,19 @@
-import { useState, useRef } from 'react';
 import SideBar from '@/components/SideBar';
 import Chats from '@/components/Chats';
 import SideBarCompact from '@/components/SideBarCompact';
 import AuthModal from '@/components/AuthModal';
-const Home = (
-    {
-        handleNewChat,
-        handleChatSelect,
-        handleDeleteChat,
-        chats,
-        currentChatID,
-        messages,
-        setMessages,
-        input,
-        setInput,
-        isLoading,
-        setIsLoading,
-        handleSendMessage,
-        isBlocked,
-        isAuth,
-        messageCount
-    }
-) => {
-    // CONSTANTES PARA MANEJAR EL ESTADO DE LOS SIDEBAR
-    const [isCompact, setIsCompact] = useState(false);
-    const [isMobileOpen, setIsMobileOpen] = useState(false);
+import { useChatContext } from '@/hooks/useChatContext';
+const Home = () => {
 
-    // CONSTANTE PARA MANEJAR EL ESTADO DEL SEARCHMENU
-    const [isMenuSearchOpen, setIsMenuSearchOpen] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
-    const searchRef = useRef(null);
+    const {
+        isAuth,
+        messageCount,
+        isCompact,
+        setIsCompact,
+        isMobileOpen,
+        setIsMobileOpen,
+        setIsMenuSearchOpen,
+    } = useChatContext();
 
     return (
         <div className="flex h-screen">
@@ -41,34 +25,14 @@ const Home = (
                 {isCompact ? (
                     <SideBarCompact
                         onExpand={() => setIsCompact(false)}
-                        onNewChat={handleNewChat}
                         onMenuSearchOpen={() => setIsMenuSearchOpen(true)}
                         onMenuSearchClose={() => setIsMenuSearchOpen(false)}
-                        isMenuSearchOpen={isMenuSearchOpen}
-                        setIsMenuSearchOpen={setIsMenuSearchOpen}
-                        searchTerm={searchTerm}
-                        setSearchTerm={setSearchTerm}
-                        onChatSelect={handleChatSelect}
-                        onDeleteChat={handleDeleteChat}
-                        chats={chats}
-                        searchRef={searchRef}
                     />
                 ) : (
                     <SideBar
                         onCollapse={() => setIsCompact(true)}
-                        chats={chats}
-                        currentChatID={currentChatID}
-                        onNewChat={handleNewChat}
-                        onChatSelect={handleChatSelect}
-                        onDeleteChat={handleDeleteChat}
                         onMenuSearchOpen={() => setIsMenuSearchOpen(true)}
                         onMenuSearchClose={() => setIsMenuSearchOpen(false)}
-                        isMenuSearchOpen={isMenuSearchOpen}
-                        setIsMenuSearchOpen={setIsMenuSearchOpen}
-                        searchTerm={searchTerm}
-                        setSearchTerm={setSearchTerm}
-                        searchRef={searchRef}
-                        setIsMobileOpen={setIsMobileOpen}
                         className="hidden md:flex"
                     />
                 )}
@@ -86,34 +50,15 @@ const Home = (
                         <div className='relative z-30 w-64 h-full bg-gray-800'>
                             <SideBar
                                 onClose={() => setIsMobileOpen(false)}
-                                setIsMobileOpen={setIsMobileOpen}
-                                chats={chats}
-                                currentChatID={currentChatID}
-                                onNewChat={handleNewChat}
-                                onChatSelect={handleChatSelect}
-                                onDeleteChat={handleDeleteChat}
                                 onMenuSearchOpen={() => setIsMenuSearchOpen(true)}
                                 onMenuSearchClose={() => setIsMenuSearchOpen(false)}
-                                isMenuSearchOpen={isMenuSearchOpen}
-                                setIsMenuSearchOpen={setIsMenuSearchOpen}
-                                searchTerm={searchTerm}
-                                setSearchTerm={setSearchTerm}
-                                searchRef={searchRef}
                             />
                         </div>
                     </div>
                 )}
             </div>
             <Chats
-                messages={messages}
-                setMessages={setMessages}
-                input={input}
-                setInput={setInput}
-                isLoading={isLoading}
-                setIsLoading={setIsLoading}
-                handleSendMessage={handleSendMessage}
                 onOpenMenu={() => setIsMobileOpen(true)}
-                isBlocked={isBlocked}
             />
         </div>
     )
