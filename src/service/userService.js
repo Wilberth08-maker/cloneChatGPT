98fb7daad923cbbe2907f3cd19748b77d1bb8d9e
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-09-02 21:07:04 -0500

    ✨refactor: mejorar formato y consistencia en axiosConfig y userService

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,31 +1,25 @@
-import axiosInstance from "./axiosConfig"
+import axiosInstance from "./axiosConfig";
 
 // ocupo mandar información
 // registar un usuario (Signup)
 // 🟢 Registrar nuevo usuario
 const registerUserService = async (data) => {
-    const response = await axiosInstance.post("/register", data)
-    return response
-}
-//http://localhost/register{json}
-
+  const response = await axiosInstance.post("/api/auth/signup", data);
+  return response;
+};
+//http://localhost/api/auth/signup{json}
 
 // 🟡 Autenticación de usuario
 const loginUserService = async (data) => {
-    const response = await axiosInstance.post("/login", data)
-    return response
-}
-//http://localhost/login{json}
+  const response = await axiosInstance.post("/api/auth/login", data);
+  return response;
+};
+//http://localhost/api/auth/login{json}
 
 // 🔵 Obtener información del usuario autenticado
 const getMeUserService = async () => {
-    const { data } = await axiosInstance.get("/users/me")
-    return data;
-}
+  const { data } = await axiosInstance.get("/users/me");
+  return data;
+};
 
-
-export {
-    registerUserService,
-    loginUserService,
-    getMeUserService
-}
+export { registerUserService, loginUserService, getMeUserService };
