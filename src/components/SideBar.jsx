7fc42568adf99e77abcb8354df9f5084ccc982e4
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-09-04 13:52:08 -0500

    ✨feat: apartado en el sideBar para acceder al dashboard del usuario

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -112,7 +112,7 @@ const SideBar = ({ className, onCollapse, onClose, onMenuSearchOpen, onMenuSearc
               <span className="flex-grow text-left">Scholar GPT</span>
             </button>
           </div>
-          <div className="flex flex-col space-y-2 itemList pr-4 pl-4 pb-4">
+          <div className="flex flex-col space-y-2 itemList pr-4 pl-4 pb-4 flex-grow">
             <h2 className='p-2 text-gray-500'>
               Chats
             </h2>
@@ -154,10 +154,10 @@ const SideBar = ({ className, onCollapse, onClose, onMenuSearchOpen, onMenuSearc
             </div>
           </div>
           {isAuth && (
-            <div className="flex sticky bottom-0 w-full p-2.5 border-t border-gray-100 dark:border-gray-700 bg-[#fafafa] dark:bg-gray-700 upperSideBottom dark:bg-gray-950 dark:text-gray-200 align-center  justify-center items-center">
+            <div className="flex sticky bottom-0 w-full p-2 border-t border-gray-100 dark:border-gray-700 bg-[#fafafa] dark:bg-gray-700 upperSideBottom dark:bg-gray-950 dark:text-gray-200 align-center  justify-center items-center">
               <Link
                 to="/dashboard"
-                className="block text-gray-800 w-full py-2 px-3 rounded-xl hover:bg-gray-200 dark:hover:bg-gray-800 dark:text-gray-200 !no-underline flex space-between gap-2">
+                className="block text-gray-800 w-full py-1 px-2 rounded-xl hover:bg-gray-200 dark:hover:bg-gray-800 dark:text-gray-200 !no-underline flex space-between gap-2">
                 <div className='flex-shrink-0 w-8 h-8 rounded-full overflow-hidden flex items-center justify-center mr-1 object-center'>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 3 24 12" fill="currentColor" className="size-8 w-full h-full object-center text-gray-800 dark:text-gray-200">
                     <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
