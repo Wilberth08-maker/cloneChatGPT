6c47f0858605328fad898740a491605b777f66c4
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-09-05 17:49:18 -0500

    ✨feat: configuración del puglin de tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
-  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       keyframes: {
-        'slide-down': {
-          '0%':   { opacity: '1', transform: 'translateY(-0.5rem)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
+        "slide-down": {
+          "0%": { opacity: "1", transform: "translateY(-0.5rem)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
       },
       animation: {
-        'slide-down': 'slide-down 250ms ease-out forwards',
+        "slide-down": "slide-down 250ms ease-out forwards",
       },
     },
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/typography")],
+};
