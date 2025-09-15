8120ff11678178ebdae6e2d6d08c0d9049bcb8eb
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-09-14 21:05:21 -0500

    💄style: mejora de estilo del nombre del chat en darkMode

--- src/components/ConfirmModal.jsx
+++ src/components/ConfirmModal.jsx
@@ -18,7 +18,7 @@ const AuthModal = () => {
             <div className="fixed inset-0 bg-gray-900 bg-opacity-25 flex items-center justify-center p-4 z-50 dark:bg-opacity-80">
                 <div className="bg-gray-50 rounded-xl shadow-xl p-6 sm:p-8 w-full max-w-sm text-center dark:bg-gray-800 dark:text-gray-200">
                     <p className="text-gray-800 dark:text-gray-100 p-2">¿Deseas eliminar el chat?</p>
-                    <p className="p-4"> Esto eliminará <strong>{chatToDelete?.title || "este chat"}</strong></p>
+                    <p className="p-4"> Esto eliminará <strong className="dark:text-gray-50">{chatToDelete?.title || "este chat"}</strong></p>
                     <div className="flex justify-center space-x-3 p-2">
                         <button
                             onClick={cancelDelete}
