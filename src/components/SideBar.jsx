62586746625ed57961e662c26073777dc8f09ec5
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-09-15 00:55:40 -0500

    💄style: mejora de estilo para el link al dashboard

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -175,14 +175,14 @@ const SideBar = ({ className, onCollapse, onClose, onMenuSearchOpen, onMenuSearc
               <Link
                 to="/dashboard"
                 className="block text-gray-800 w-full py-1 px-2 rounded-xl hover:bg-gray-200 dark:hover:bg-gray-800 dark:text-gray-200 !no-underline flex space-between gap-2">
-                <div className='flex-shrink-0 w-8 h-8 rounded-full overflow-hidden flex items-center justify-center mr-1 object-center'>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 3 24 12" fill="currentColor" className="size-8 w-full h-full object-center text-gray-800 dark:text-gray-200">
+                <div className='flex-shrink-0 w-8 h-8 rounded-full overflow-hidden flex items-center justify-center object-center'>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 3 24 15" fill="currentColor" className="size-8 w-full h-full object-center text-gray-800 dark:text-gray-200">
                     <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
                   </svg>
                 </div>
                 <div className='align-column'>
-                  <p className='text-base text-gray-800 dark:text-gray-200'>{userPayload.email}</p>
-                  <p className='text-sm text-gray-600 dark-text-gray-400'>gratis</p>
+                  <p className='text-sm text-gray-800 dark:text-gray-200'>{userPayload.email}</p>
+                  <p className='text-xs text-gray-600 dark-text-gray-400'>Gratis</p>
                 </div>
 
               </Link>
